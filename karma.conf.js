86d41b66d8b6c98641d1856e14f5b5aea14a582d
Josh Bebbington <josh@focalwebsites.com>
2015-03-23 21:05:47 +0000

    Creates a blank angular/karma app

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+// Karma configuration
+// Generated on Mon Mar 23 2015 21:02:48 GMT+0000 (GMT)
+
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/**/*.js',
+      'spec/**/*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
